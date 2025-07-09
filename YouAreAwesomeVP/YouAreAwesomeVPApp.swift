51e7ce6cfb4e5ce14dff73ac2d29a7e5e83532e8
Bob Witmer <bwitmer@golden.net>
2025-07-09 16:57:07 -0400

    Initial Commit

---
+++ YouAreAwesomeVP/YouAreAwesomeVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeVPApp.swift
+//  YouAreAwesomeVP
+//
+//  Created by Bob Witmer on 2025-07-09.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YouAreAwesomeVPTests/YouAreAwesomeVPTests.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeVPTests.swift
+//  YouAreAwesomeVPTests
+//
+//  Created by Bob Witmer on 2025-07-09.
+//
+
+import Testing
+@testable import YouAreAwesomeVP
+
+struct YouAreAwesomeVPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
